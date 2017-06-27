c542ea7e451d31717729f7b2c76e389c61b4b19d
Kenan Yildirim <kenan@kenany.me>
2017-06-27 14:07:06 -0400

    add new emote downloading script
    Closes https://github.com/KenanY/destiny.gg-emotes/issues/4

---
+++ scripts/download-emotes.js
@@ -0,0 +1,14 @@
+const get = require('simple-get');
+const process = require('process');
+
+const opts = {
+  url: 'https://rawgit.com/destinygg/website/master/assets/emotes.json',
+  json: true
+};
+
+get.concat(opts, (error, res, data) => {
+  if (error) {
+    throw error;
+  }
+  process.stdout.write(JSON.stringify(data.destiny.concat(data.twitch)));
+});
